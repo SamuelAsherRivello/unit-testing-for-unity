831ed0dc11119f69b64f12c36c504abf1eb104f0
Samuel Asher Rivello <63511769+SamuelAsherRivello@users.noreply.github.com>
2023-11-06 15:17:48 -0300

    Unit Testing: Folder Structure & First Sample(s)

---
+++ Unity/Assets/Samples/Shared/Scripts/Runtime/RMC/Templates/ITemplateInterface.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace RMC.UnitTesting.Shared.Templates
+{
+	/// <summary>
+	/// Replace with comments...
+	/// </summary>
+	public interface ITemplateInterface
+	{
+		//  Properties ------------------------------------
+		int SampleProperty { get; set; }
+
+		//  Methods ---------------------------------------
+		void SampleMethod(int argument_int);
+	}
+}
